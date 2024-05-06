c81f5c2e4c6da67f0ce82cbc14716fc6d9f47b12
Linus Ros <linus.ros@prevas.se>
2024-05-06 08:25:45 +0200

    Minor fix

--- python/constellation/satellites/rpsatellite.py
+++ python/constellation/satellites/rpsatellite.py
@@ -263,15 +263,16 @@ class RedPitayaSatellite(DataSender):
 
             # Append last part of buffer before resetting
             if cycled:
-                buffer = np.concatenate(
+                buffer = np.concatenate((
                     self._sample_raw32(
                         start=self._readpos,
                         stop=BUFFER_SIZE,
-                        channel=channel),
+                        channel=channel,
+                    ),
                     self._sample_raw32(
                         start=0,
                         stop=self._writepos,
-                        channel=channel))
+                        channel=channel)))
             else:
                 buffer = self._sample_raw32(
                     start=self._readpos,
