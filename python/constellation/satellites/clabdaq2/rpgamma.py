1803823491f3ee24c0521695d6621f7c62b7a639
Hanno Perrey <hanno.perrey@dvel.se>
2024-05-28 13:25:53 +0200

    rp: fix import

--- python/constellation/satellites/clabdaq2/rpgamma.py
+++ python/constellation/satellites/clabdaq2/rpgamma.py
@@ -14,7 +14,7 @@ import numpy as np
 import rp
 
 from ..core.satellite import SatelliteArgumentParser
-from .base import setup_cli_logging
+from ..base import setup_cli_logging
 from constellation.core.configuration import ConfigError
 from .rpsatellite import RedPitayaSatellite, axi_regset_start_stop
 
